54803bbb7d3945a3ff4233c2c3fb79a1c6fc6946
Matej Gomboc <matejg03@gmail.com>
2016-06-11 19:55:43 +0200

    Update

---
+++ PC user interface/impedance_meter_CLI/impedance_meter_CLI/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using small_impedance_meter;
+
+namespace impedance_meter_CLI
+{
+    internal class Program
+    {
+
+        private static void Main(string[] args)
+        {
+            SmallImpedanceMeter smallImpedanceMeter = new SmallImpedanceMeter();
+            smallImpedanceMeter.OnDeviceAttached += OnDeviceAttached;
+            smallImpedanceMeter.OnDeviceRemoved += OnDeviceRemoved;
+
+            Console.WriteLine("Waiting for device ...");
+
+            while (!Console.KeyAvailable) Application.DoEvents();
+
+            smallImpedanceMeter.OnDeviceAttached -= OnDeviceAttached;
+            smallImpedanceMeter.OnDeviceRemoved -= OnDeviceRemoved;
+        }
+
+        private static void OnDeviceAttached(object sender, EventArgs e)
+        {
+            // A Device system-level event has occured
+
+            Console.SetCursorPosition(0, Console.CursorTop);
+            Console.WriteLine("Device Attached.");
+        }
+
+        private static void OnDeviceRemoved(object sender, EventArgs e)
+        {
+            // A Device system-level event has occured
+
+            Console.SetCursorPosition(0, Console.CursorTop);
+            Console.WriteLine("Device Removed.");
+        }
+    }
+}
