9e8e80d2461857bccf6a0eb82569f5fee23b7719
Matej Gomboc <matejg03@gmail.com>
2016-07-31 11:42:42 +0200

    Update

--- PC user interface/impedance meter CLI/impedance_meter_CLI/Program.cs
+++ PC user interface/impedance meter CLI/impedance_meter_CLI/Program.cs
@@ -30,30 +30,13 @@ namespace impedance_meter_CLI
 
             try
             {
-                smallImpedanceMeter.AD5933_CheckFrequencySweepComplete();
-                smallImpedanceMeter.AD5933_CheckValidData();
-                smallImpedanceMeter.AD5933_CheckValidTemp();
-                smallImpedanceMeter.AD5933_GetFreqIncrement();
-                smallImpedanceMeter.AD5933_GetImaginaryValue();
-                smallImpedanceMeter.AD5933_GetNumOfIncrements();
-                smallImpedanceMeter.AD5933_GetNumOfSettlingCycles();
-                smallImpedanceMeter.AD5933_GetRealValue();
-                smallImpedanceMeter.AD5933_GetStartFreq();
-                smallImpedanceMeter.AD5933_GetTemp();
-                smallImpedanceMeter.AD5933_IncrementFreq();
-                smallImpedanceMeter.AD5933_Init();
-                smallImpedanceMeter.AD5933_InitWithStartFreq();
-                smallImpedanceMeter.AD5933_Powerdown();
-                smallImpedanceMeter.AD5933_RepeatFreq();
-                smallImpedanceMeter.AD5933_Reset();
-                smallImpedanceMeter.AD5933_Standby();
+                smallImpedanceMeter.SetClockDivider(0);
                 smallImpedanceMeter.SetToCalibrate();
-                smallImpedanceMeter.SetToMeasure();
-                smallImpedanceMeter.Ping();
-                smallImpedanceMeter.InitClockDivider();
-                
 
-                //smallImpedanceMeter.AD5933_MeasureTemp();
+                //smallImpedanceMeter.AD5933_SelectVoutRange(AD5933_VoutRange.Range_200mVpp);
+                //Thread.Sleep(1000);
+
+                DumpRegisterData();
             }
             catch (Exception ex)
             {
@@ -66,7 +49,7 @@ namespace impedance_meter_CLI
             smallImpedanceMeter.OnDeviceRemoved -= OnDeviceRemoved;
 
             Console.WriteLine("Device Disconnected.");
-
+            
             Thread.Sleep(2000);
         }
 
@@ -84,6 +67,101 @@ namespace impedance_meter_CLI
 
             Console.SetCursorPosition(0, Console.CursorTop);
             Console.WriteLine("Device Removed.");
+        }
+
+        private static string ToBinaryString(byte decimalNumber)
+        {
+            int remainder = 0;
+            string result = string.Empty;
+
+            if (decimalNumber == 0) result = "0";
+
+            while (decimalNumber > 0)
+            {
+                remainder = decimalNumber % 2;
+                decimalNumber /= 2;
+                result = remainder.ToString() + result;
+            }
+
+            result = result.PadLeft(8, '0');
+            return result;
+        }
+
+        private static void DumpRegisterData()
+        {
+            Console.WriteLine();
+            Console.WriteLine("ctrl:");
+            Console.WriteLine(ToBinaryString(smallImpedanceMeter.AD5933_RegisterRead(0x80)));
+            Console.WriteLine(ToBinaryString(smallImpedanceMeter.AD5933_RegisterRead(0x81)));
+            Console.WriteLine();
+            Console.WriteLine(smallImpedanceMeter.AD5933_RegisterRead(0x80));
+            Console.WriteLine(smallImpedanceMeter.AD5933_RegisterRead(0x81));
+
+            Console.WriteLine();
+            Console.WriteLine("start freq:");
+            Console.WriteLine(ToBinaryString(smallImpedanceMeter.AD5933_RegisterRead(0x82)));
+            Console.WriteLine(ToBinaryString(smallImpedanceMeter.AD5933_RegisterRead(0x83)));
+            Console.WriteLine(ToBinaryString(smallImpedanceMeter.AD5933_RegisterRead(0x84)));
+            Console.WriteLine();
+            Console.WriteLine(smallImpedanceMeter.AD5933_RegisterRead(0x82));
+            Console.WriteLine(smallImpedanceMeter.AD5933_RegisterRead(0x83));
+            Console.WriteLine(smallImpedanceMeter.AD5933_RegisterRead(0x84));
+
+            Console.WriteLine();
+            Console.WriteLine("freq incrm:");
+            Console.WriteLine(ToBinaryString(smallImpedanceMeter.AD5933_RegisterRead(0x85)));
+            Console.WriteLine(ToBinaryString(smallImpedanceMeter.AD5933_RegisterRead(0x86)));
+            Console.WriteLine(ToBinaryString(smallImpedanceMeter.AD5933_RegisterRead(0x87)));
+            Console.WriteLine();
+            Console.WriteLine(smallImpedanceMeter.AD5933_RegisterRead(0x85));
+            Console.WriteLine(smallImpedanceMeter.AD5933_RegisterRead(0x86));
+            Console.WriteLine(smallImpedanceMeter.AD5933_RegisterRead(0x87));
+
+            Console.WriteLine();
+            Console.WriteLine("num of incrm:");
+            Console.WriteLine(ToBinaryString(smallImpedanceMeter.AD5933_RegisterRead(0x88)));
+            Console.WriteLine(ToBinaryString(smallImpedanceMeter.AD5933_RegisterRead(0x89)));
+            Console.WriteLine();
+            Console.WriteLine(smallImpedanceMeter.AD5933_RegisterRead(0x88));
+            Console.WriteLine(smallImpedanceMeter.AD5933_RegisterRead(0x89));
+
+            Console.WriteLine();
+            Console.WriteLine("num of settl:");
+            Console.WriteLine(ToBinaryString(smallImpedanceMeter.AD5933_RegisterRead(0x8A)));
+            Console.WriteLine(ToBinaryString(smallImpedanceMeter.AD5933_RegisterRead(0x8B)));
+            Console.WriteLine();
+            Console.WriteLine(smallImpedanceMeter.AD5933_RegisterRead(0x8A));
+            Console.WriteLine(smallImpedanceMeter.AD5933_RegisterRead(0x8B));
+
+            Console.WriteLine();
+            Console.WriteLine("stat:");
+            Console.WriteLine(ToBinaryString(smallImpedanceMeter.AD5933_RegisterRead(0x8F)));
+            Console.WriteLine();
+            Console.WriteLine(smallImpedanceMeter.AD5933_RegisterRead(0x8F));
+
+            Console.WriteLine();
+            Console.WriteLine("temp:");
+            Console.WriteLine(ToBinaryString(smallImpedanceMeter.AD5933_RegisterRead(0x92)));
+            Console.WriteLine(ToBinaryString(smallImpedanceMeter.AD5933_RegisterRead(0x93)));
+            Console.WriteLine();
+            Console.WriteLine(smallImpedanceMeter.AD5933_RegisterRead(0x92));
+            Console.WriteLine(smallImpedanceMeter.AD5933_RegisterRead(0x93));
+
+            Console.WriteLine();
+            Console.WriteLine("real:");
+            Console.WriteLine(ToBinaryString(smallImpedanceMeter.AD5933_RegisterRead(0x94)));
+            Console.WriteLine(ToBinaryString(smallImpedanceMeter.AD5933_RegisterRead(0x95)));
+            Console.WriteLine();
+            Console.WriteLine(smallImpedanceMeter.AD5933_RegisterRead(0x94));
+            Console.WriteLine(smallImpedanceMeter.AD5933_RegisterRead(0x95));
+
+            Console.WriteLine();
+            Console.WriteLine("imag:");
+            Console.WriteLine(ToBinaryString(smallImpedanceMeter.AD5933_RegisterRead(0x96)));
+            Console.WriteLine(ToBinaryString(smallImpedanceMeter.AD5933_RegisterRead(0x97)));
+            Console.WriteLine();
+            Console.WriteLine(smallImpedanceMeter.AD5933_RegisterRead(0x96));
+            Console.WriteLine(smallImpedanceMeter.AD5933_RegisterRead(0x97));
         }
     }
 }
